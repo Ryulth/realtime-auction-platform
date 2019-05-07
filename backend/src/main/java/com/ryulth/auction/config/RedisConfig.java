7cf197dbcb57a38a5ef0b150a3617221b6b56114
Ryulth <adboy94@khu.ac.kr>
2019-05-07 23:46:02 +0900

    RedisConfig 작성 & 디펜던시 추가 & local properties file 추가

---
+++ backend/src/main/java/com/ryulth/auction/config/RedisConfig.java
@@ -0,0 +1,49 @@
+package com.ryulth.auction.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.clients.jedis.JedisPoolConfig;
+
+@Configuration
+public class RedisConfig {
+
+    private @Value("${spring.redis.host}") String redisHost;
+    private @Value("${spring.redis.port}") int redisPort;
+    private @Value("${spring.redis.password}") String password;
+    @Bean
+    public JedisPoolConfig jedisPoolConfig() {
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setTestOnBorrow(true);
+        jedisPoolConfig.setTestOnReturn(true);
+        return jedisPoolConfig;
+    }
+
+    @SuppressWarnings("deprecation")
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory(JedisPoolConfig jedisPoolConfig) {
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(jedisPoolConfig);
+        jedisConnectionFactory.setHostName(redisHost);
+        jedisConnectionFactory.setPort(redisPort);
+        jedisConnectionFactory.setPassword(password);
+        jedisConnectionFactory.setUsePool(true);
+        return jedisConnectionFactory;
+    }
+
+    @Bean(name="redisTemplate")
+    public RedisTemplate redisTemplateConfig(JedisConnectionFactory jedisConnectionFactory) {
+        RedisTemplate redisTemplate = new RedisTemplate();
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setConnectionFactory(jedisConnectionFactory);
+        redisTemplate.setEnableDefaultSerializer(false);
+        redisTemplate.setEnableTransactionSupport(true);
+
+        return redisTemplate;
+    }
+}
