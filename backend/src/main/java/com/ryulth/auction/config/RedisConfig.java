aa98f8aaf9245d1249f14b92604d1dffa7f931ea
Ryulth <adboy94@khu.ac.kr>
2019-05-11 19:11:06 +0900

    Jedis를 lettuce 로 변경& spring-data-redis 2.2.0SNAPSHOT 버젼으로 변경  -> redis 5.0 기능인  stream xadd 를 redistemplate 을 사용하기 위해 변경

--- backend/src/main/java/com/ryulth/auction/config/RedisConfig.java
+++ backend/src/main/java/com/ryulth/auction/config/RedisConfig.java
@@ -3,52 +3,39 @@ package com.ryulth.auction.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisPassword;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import redis.clients.jedis.JedisPoolConfig;
-
-import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
 
-    private @Value("${spring.redis.host}") String redisHost;
-    private @Value("${spring.redis.port}") int redisPort;
-    private @Value("${spring.redis.password}") String password;
+    private @Value("${spring.redis.host}")
+    String redisHost;
+    private @Value("${spring.redis.port}")
+    int redisPort;
+    private @Value("${spring.redis.password}")
+    String password;
+
     @Bean
-    public JedisPoolConfig jedisPoolConfig() {
-        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
-        jedisPoolConfig.setTestOnBorrow(true);
-        jedisPoolConfig.setTestOnReturn(true);
-        return jedisPoolConfig;
-    }
-
-    @SuppressWarnings("deprecation")
-    @Bean
-    public JedisConnectionFactory jedisConnectionFactory(JedisPoolConfig jedisPoolConfig) {
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(redisHost,redisPort);
-        redisStandaloneConfiguration.setPassword(RedisPassword.of(password));
-
-        JedisClientConfiguration.JedisClientConfigurationBuilder jedisClientConfigurationBuilder = JedisClientConfiguration.builder();
-        jedisClientConfigurationBuilder.connectTimeout(Duration.ofSeconds(1));
-        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(redisStandaloneConfiguration, jedisClientConfigurationBuilder.build());
-
-        return jedisConnectionFactory;
+    public RedisConnectionFactory redisConnectionFactory() {
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory();
+        return lettuceConnectionFactory;
     }
 
     @Bean(name="redisTemplate")
-    public RedisTemplate redisTemplateConfig(JedisConnectionFactory jedisConnectionFactory) {
-        RedisTemplate redisTemplate = new RedisTemplate();
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        redisTemplate.setConnectionFactory(jedisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setEnableDefaultSerializer(false);
         redisTemplate.setEnableTransactionSupport(true);
         return redisTemplate;
     }
+
+
 }
--- backend/src/main/java/com/ryulth/auction/controller/ProductController.java
+++ backend/src/main/java/com/ryulth/auction/controller/ProductController.java
@@ -1,16 +1,28 @@
 package com.ryulth.auction.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.ryulth.auction.pojo.response.ProductListResponse;
 import com.ryulth.auction.pojo.response.ProductDetailResponse;
+import com.ryulth.auction.pojo.response.ProductListResponse;
 import com.ryulth.auction.service.product.ProductService;
+
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.XAddArgs;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisStreamCommands;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class ProductController {
