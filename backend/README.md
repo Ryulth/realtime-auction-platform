fac9eab34fff94e64d5f2494ca884c02122888af
Ryulth <adboy94@khu.ac.kr>
2019-05-12 09:56:53 +0900

    opsForStream 설정 파일 세팅 완료 및 작동 확인 spring-data-* 2.2.0.BUILD_SNAPSHOT 버젼 사용

--- backend/README.md
+++ backend/README.md
@@ -4,10 +4,10 @@
 
 * Java 8 (Spring Boot 2+)
 * Jpa
-
 * MySql 5.7
-* Redis 
-
+* Redis 5.0 +
+  * Streams 사용 [docs](<https://github.com/spring-projects/spring-data-redis/blob/master/src/main/asciidoc/reference/redis-streams.adoc>)
+  * [XADD](<https://redis.io/commands/xadd>)
 * *Deploy 시 (nginx & docker)
 
 #### Technique
--- backend/src/main/java/com/ryulth/auction/controller/ProductController.java
+++ backend/src/main/java/com/ryulth/auction/controller/ProductController.java
@@ -4,16 +4,12 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.ryulth.auction.pojo.response.ProductDetailResponse;
 import com.ryulth.auction.pojo.response.ProductListResponse;
 import com.ryulth.auction.service.product.ProductService;
-
-import io.lettuce.core.RedisClient;
-import io.lettuce.core.XAddArgs;
-import io.lettuce.core.api.StatefulRedisConnection;
-import io.lettuce.core.api.sync.RedisStreamCommands;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StreamOperations;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -59,4 +55,25 @@ public class ProductController {
         return productService.deleteProduct(productId);
     }
 
+    @Autowired
+    RedisTemplate redisTemplate;
+    private  int temp = 0;
+    @GetMapping("/redisTest")
+    public void redisTest() {
+        ValueOperations vop = redisTemplate.opsForValue();
+        ListOperations lop = redisTemplate.opsForList();
+        List a = lop.range("test", 1, 200);
+        System.out.println(a);
+        vop.set("jdkSerial", "jdk");
+        temp += 1;
+        lop.rightPush("test", temp);
+        String result = (String) vop.get("jdkSerial");
+        int result2 = (int) lop.index("test", -1);
+        System.out.println(result);//jdk
+        System.out.println(result2);//jdk
+        System.out.println(lop.size("test"));
+        StreamOperations sop = redisTemplate.opsForStream();
+        Map<String, String> body = Collections.singletonMap("time", LocalDateTime.now().toString());
+        sop.add("streamTest",body);
+    }
 }
